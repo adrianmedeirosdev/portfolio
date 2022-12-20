9e17e58de56ba61a1ee2cc98a996f0bb32a74738
Adrian <lima.adrian13@gmail.comgit config -- global user.name Adrian>
2022-12-19 22:57:46 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const navList = document.querySelector(".nav-list")
+const navItems = navList.querySelectorAll("li")
+
+
+navItems.forEach(navItem => {
+  navItem.addEventListener("click", () => {
+    var current = document.querySelectorAll('.active')
+    current[0].className = current[0].className.replace("active", "")
+    if (!navItem.classList.contains("active")) {
+      navItem.classList.add("active")
+    }
+  })
+})
+
+
+
+
+
+
